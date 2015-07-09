1d9786c62128924c0b77b51aa168133102fa4376
Toni Carbone <kokorito@gmx.de>
2015-07-10 01:10:46 +0200

    it works

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,18 +1,5 @@
-$("#main").append("Toni Carbone ");
-
-
-var awesomeThoughts = "I'm Tony and baähm!!";
-var funThoughts = awesomeThoughts.replace("baähm", "geil");
 var formattedName = "Tony Carbone";
 var formattedRole = "Whatever you want";
-console.log(awesomeThoughts);
-console.log(funThoughts);
-
-$('#main').append(funThoughts);
-
-
 
 $('#header').append(HTMLheaderName.replace("%data%", formattedName));
 $('#header').append(HTMLheaderRole.replace("%data%", formattedRole));
-
-
